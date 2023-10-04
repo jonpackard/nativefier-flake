f0928cecac71404c21e370f404828a5e7553230f
Jonathan Packard <jwpackard@duck.com>
2023-10-03 23:11:59 -0400

    Fix missing space in PS1

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
         ];
         
         shellHook = ''
-          printf "\n*** Note: On the first run, it can take a while to download packages! ***\n" && npm install -i nativefier && alias nativefier="./node_modules/.bin/nativefier" && export PS1="\n\[\033[1;32m\][nix-shell-nativefier:\w]\$\[\033[0m\]" && printf "\n*** Nativefier installation complete. Created alias "nativefier" that will launch nativefier from this path. ***\n"
+          printf "\n*** Note: On the first run, it can take a while to download packages! ***\n" && npm install -i nativefier && alias nativefier="./node_modules/.bin/nativefier" && export PS1="\n\[\033[1;32m\][nix-shell-nativefier:\w]\$\[\033[0m\] " && printf "\n*** Nativefier installation complete. Created alias "nativefier" that will launch nativefier from this path. ***\n"
         '';
 
       };
